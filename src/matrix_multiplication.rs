b6dfbb35f0d861455b43446e67716f392b5a89dc
MCdeamon7 <micro.97@hotmail.it>
2023-01-19 18:12:43 +0100

    optimized and unoptimized sequantial

--- src/matrix_multiplication.rs
+++ src/matrix_multiplication.rs
@@ -12,6 +12,27 @@ pub mod generate;
 mod sanitize;
 mod types;
 
+/*pub fn matrix_multiplication_sequential_ijk_unoptimized(
+    a: &Vec<Vec<i32>>,
+    b: &Vec<Vec<i32>>,
+) -> Result<Vec<Vec<i32>>, SanitizeError> {
+    sanitize_matrices(a, b)?;
+
+    let size = a.len();
+
+    let mut c = zero_filled_square_matrix_of_size!(size);
+
+    for i in 0..size {
+        for j in 0..size {
+            for k in 0..size {
+                c[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+
+    Ok(c)
+}*/
+
 pub fn matrix_multiplication_sequential_ijk(
     a: &Vec<Vec<i32>>,
     b: &Vec<Vec<i32>>,
@@ -39,6 +60,27 @@ pub fn matrix_multiplication_sequential_ijk(
 
     Ok(c)
 }
+
+/*pub fn matrix_multiplication_sequential_ikj_unoptimized(
+    a: &Vec<Vec<i32>>,
+    b: &Vec<Vec<i32>>,
+) -> Result<Vec<Vec<i32>>, SanitizeError> {
+    sanitize_matrices(a, b)?;
+
+    let size = a.len();
+
+    let mut c = zero_filled_square_matrix_of_size!(size);
+
+    for i in 0..size {
+        for k in 0..size {
+            for j in 0..size {
+                c[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+
+    Ok(c)
+}*/
 
 pub fn matrix_multiplication_sequential_ikj(
     a: &Vec<Vec<i32>>,
